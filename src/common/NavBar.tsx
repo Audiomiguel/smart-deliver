12098c65d29c0514de1fd89640b3f5059c14b483
Miguel Bustillos <miguel.bustillos@onroadts.com>
2023-11-12 09:09:19 -0500

    rename admin to login

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -33,7 +33,7 @@ export const NavBar = () => {
                     variant="outlined"
                     color="inherit"
                   >
-                    Admin
+                    Login
                   </Button>
                   {/* <MetaMaskButton /> */}
                 </Stack>
